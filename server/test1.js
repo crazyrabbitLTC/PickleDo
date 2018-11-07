de6cd493ddc2e69b805710b977a25c6368024901
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-07 15:38:31 -0500

    create a new test

--- server/test1.js
+++ server/test1.js
@@ -1,15 +1,18 @@
 "use strict";
 
-const fs = require("fs");
+
 const HDWalletPRovider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const memonic = "detail august fragile luggage coyote home trap veteran witness result feed blade";
-const privKey = "62b8292bc6e27d594b7bf4f71bcb79c85e26cd506704c3f14d21ed1e17cfd9d3";
-const provider = new HDWalletPRovider(privKey, "http://localhost:7545", 5);
-const web3 = new Web3(provider);
-
 const artifacts = require('../build/contracts/testToken.json')
 const contract = require('truffle-contract')
+
+const privKey = "62b8292bc6e27d594b7bf4f71bcb79c85e26cd506704c3f14d21ed1e17cfd9d3";
+const privateKey = Buffer.from(privKey, "hex");
+const provider = new HDWalletPRovider(privateKey, "http://localhost:7545");
+const web3 = new Web3(provider);
+
+
 
 let testToken = contract(artifacts);
 
@@ -30,7 +33,8 @@ const deploy = async () => {
   try {
   const mint = await myContract.mintWithTokenURI("0x4A3EAeA9f76E26084520926EeC8fCd90d1F08a69", 0, "This is the first token", {from: accounts[0], gas: 300000});
   const totalSupply = await myContract.totalSupply.call();
-  console.log("Total Supply: ", totalSupply.toString());
+  const name = await myContract.name.call();
+  console.log("Total Supply: ", totalSupply.toString(), "Name: ", name);
 
   } catch (err) {
       console.log(err);
