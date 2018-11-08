7a6d1263c5fc8704a5739f546f083d4ae094401c
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-08 11:54:09 -0500

    added accounts

--- server/test2.js
+++ server/test2.js
@@ -4,13 +4,15 @@ const Web3 = require("web3");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Tx = require('ethereumjs-tx');
 const testToken = require("../build/contracts/testToken");
+const memonic = "detail august fragile luggage coyote home trap veteran witness result feed blade";
 
 const provider = new HDWalletProvider(
-  "detail august fragile luggage coyote home trap veteran witness result feed blade",
+  memonic,
   "HTTP://127.0.0.1:7545"
 );
 
 const myAddress = "0x2cA4488037250f9453032aA8dE9bE5786c5c178B";
+
 const privateKey = Buffer.from(
   "62b8292bc6e27d594b7bf4f71bcb79c85e26cd506704c3f14d21ed1e17cfd9d3",
   "hex"
@@ -21,16 +23,21 @@ const contractAddress = "0x6f129fed5004a2a37bcb0965f2a81e2f02f20783";
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
+
+
 let count;
 
 const deploy = async (uri) => {
   const balanceOf = await contract.methods.balanceOf(myAddress).call();
+  const accounts = await  web3.eth.getAccounts();
   let totalCount = await contract.methods.totalSupply.call();
+
 
   let count = await web3.eth.getTransactionCount(myAddress);
 
+  console.log("Accounts: ", accounts);
   console.log("Count: ", count);
-  console.log("TotalSupply: ", totalCount);
+  //console.log("TotalSupply: ", totalCount);
 
   var rawTransaction = {
     from: myAddress,
@@ -44,7 +51,7 @@ const deploy = async (uri) => {
     nonce: web3.utils.toHex(count)
   };
 
-  console.log("Raw Transaction: ", rawTransaction);
+  //console.log("Raw Transaction: ", rawTransaction);
 
   let transaction = new Tx(rawTransaction);
   transaction.sign(privateKey);
