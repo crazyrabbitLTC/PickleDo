30b22f88c9c7e687e67b6f83df6ece9b05671e74
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-10 07:05:39 -0500

    Simple space.

--- server/fileCheck.js
+++ server/fileCheck.js
@@ -13,6 +13,8 @@ var watcher = chokidar.watch('.', {
     ignored: /(^|[\/\\])\../,
     persistent: true
   });
+
+  
   // Something to use when events are received.
 var log = console.log.bind(console);
 
