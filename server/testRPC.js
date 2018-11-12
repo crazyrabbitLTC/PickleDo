1787f8da65eb41c7c6b484d2919f89bf676832f0
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-12 18:10:30 -0500

    testRPC called from test script

--- server/testRPC.js
+++ server/testRPC.js
@@ -1,10 +1,13 @@
 'use strict';
 
+
 const fs = require('fs');
 const spawn = require('child_process').spawn;
 
 
 console.log("Starting Ganache-CLI");
+
+//This Memonic is not secure.
 const memonic = "detail august fragile luggage coyote home trap veteran witness result feed blade";
 const port = 7545
 const ganache = spawn('ganache-cli', ["-m", memonic, "-p", port]);
